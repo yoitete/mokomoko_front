2937871a61df61d5a6ec27f0363ea040d0857621
yoitete <>
2025-09-17 23:10:49 +0900

    一旦青で仮置き

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,15 +6,22 @@ import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Footer() {
+  const pathname = usePathname();
+
+  const homePath = ["/home", "/christmas", "/exam-support"];
+
   return (
     <div className="sticky bg-[#7E6565] p-4 flex justify-between items-center z-50 bottom-0">
       <Link href="/home">
         <FontAwesomeIcon
           icon={faHouse}
           size="2x"
-          className="cursor-pointer text-[#F1F6F7]"
+          className={`cursor-pointer text-[#F1F6F7] ${
+            homePath.includes(pathname) ? "text-sky-300" : ""
+          }`}
         />
       </Link>
 
@@ -22,7 +29,9 @@ export default function Footer() {
         <FontAwesomeIcon
           icon={faMagnifyingGlass}
           size="2x"
-          className="cursor-pointer text-[#F1F6F7]"
+          className={`cursor-pointer text-[#F1F6F7] ${
+            pathname === "/search" ? "text-sky-300" : ""
+          }`}
         />
       </Link>
 
@@ -30,7 +39,9 @@ export default function Footer() {
         <FontAwesomeIcon
           icon={faHeart}
           size="2x"
-          className="cursor-pointer text-[#F1F6F7]"
+          className={`cursor-pointer text-[#F1F6F7] ${
+            pathname === "/favorite" ? "text-sky-300" : ""
+          }`}
         />
       </Link>
 
@@ -38,7 +49,9 @@ export default function Footer() {
         <FontAwesomeIcon
           icon={faPenToSquare}
           size="2x"
-          className="cursor-pointer text-[#F1F6F7]"
+          className={`cursor-pointer text-[#F1F6F7] ${
+            pathname === "/post" ? "text-sky-300" : ""
+          }`}
         />
       </Link>
 
@@ -46,7 +59,9 @@ export default function Footer() {
         <FontAwesomeIcon
           icon={faUser}
           size="2x"
-          className="cursor-pointer text-[#F1F6F7]"
+          className={`cursor-pointer text-[#F1F6F7] ${
+            pathname === "/mypage" ? "text-sky-300" : ""
+          }`}
         />
       </Link>
     </div>
